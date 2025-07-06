f5e701622f1c143c207ac9ec4870083cebfed385
igorms-pro <igorms.pro@gmail.com>
2025-07-06 18:19:04 +0200

    issue 1: ci fix 1.3

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
-    exclude: ['e2e', 'node_modules', 'src/types', 'src/styles', 'src/**/__tests__/**'],
+    exclude: ['e2e', 'node_modules', 'src/types', 'src/styles'],
     coverage: {
       provider: 'v8',
       reportsDirectory: './coverage',
