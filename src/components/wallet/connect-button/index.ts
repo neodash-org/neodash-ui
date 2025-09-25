35e23a6d87f29ea0e5497b6b05d5f7e093876738
igorms-pro <igorms.pro@gmail.com>
2025-09-25 21:21:57 +0200

    fix: correct BaseConnectButton export in index.ts

--- src/components/wallet/connect-button/index.ts
+++ src/components/wallet/connect-button/index.ts
@@ -1,5 +1,5 @@
 export { ChainIcon } from './ChainIcon';
-export { ConnectButton as BaseConnectButton } from './BaseConnectButton';
+export { BaseConnectButton } from './BaseConnectButton';
 export { WrongNetworkButton } from './WrongNetworkButton';
 export { ChainButton } from './ChainButton';
 export { AccountButton } from './AccountButton';
